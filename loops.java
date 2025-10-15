6a0fcd1c1edc041f500d817393fa47849ec9ca64
pravexx <sundharamhar417@gmail.com>
2025-10-15 23:20:54 +0530

    loops.java file modified

--- loops.java
+++ loops.java
@@ -1,10 +1,23 @@
-public class loops {
+// public class loops {
+//     public static void main(String[] args) {
+//         String name = "praveen";
+//         String rev = "";
+//         for(int i=name.length()-1;i>=0;i--){
+//             rev = rev + name.charAt(i);
+//         }
+//         System.out.println(rev);
+//     }
+// }
+
+public  class loops{
     public static void main(String[] args) {
-        String name = "praveen";
-        String rev = "";
-        for(int i=name.length()-1;i>=0;i--){
-            rev = rev + name.charAt(i);
+        String name = "kamatchi";
+        int count = 0;
+        for(int i=0; i<=name.length()-1;i++){
+            if('a'==name.charAt(i)){
+                count++;
+            }
         }
-        System.out.println(rev);
+        System.out.println(count);
     }
 }
